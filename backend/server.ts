2310db505262b8e83344b75e4e3c53e474482338
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2024-03-20 10:44:14 +0500

    Connected To Database
    - Installed packaged : mongodb and mogoose
    - Establisheda connection to the database

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 //////////
 dotenv.config();
 
 const app = express();
 
+const DB: string = process.env.DATABASE || "";
 const PORT = process.env.PORT;
 
+//////Database connection//////
+if (!DB) throw new Error("DATABASE environment variable is not defined");
+mongoose.connect(DB).then(() => console.log("connect to db"));
+
+////listen to port////////
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
