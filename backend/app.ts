76f13325f63b257326f9fa71ce8084dad1c47f49
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2024-04-02 15:22:24 +0500

    [FR-026] Enhanced Security with Security Packages
    - Added security packages (Helmet, HPP, ExpressMongoSanitize, rateLimit) to improve application security by setting HTTP headers, preventing HTTP parameter pollution, sanitizing MongoDB input, and implementing rate limiting.

--- backend/app.ts
+++ backend/app.ts
@@ -3,10 +3,34 @@ import productRoute from "./route/ProductRoute";
 import userRoute from "./route/UserRoute";
 import AppError from "./utils/AppError";
 import { globalErrorHandler } from "./controller/errorController";
+import { rateLimit } from "express-rate-limit";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
 
 const app = express();
 
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
+
+// Applying the rate limiting middleware to all requests.
+app.use("/api", limiter);
+
 app.use(express.json());
+
+app.use(ExpressMongoSanitize());
+
+app.use(
+  hpp({
+    whitelist: ["category", "price", "AvgRating", "tags"],
+  })
+);
 
 app.use("/api/v1/products", productRoute);
 app.use("/api/v1/user", userRoute);
